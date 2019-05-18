b84df78f278aee4d74fec2efa9fc58954ac1b1cf
chocolandtv <moegenburg@gmx.de>
2019-05-18 22:29:21 +0200

    Initial

---
+++ Assets/Scripts 1/AudioControlled.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioControlled : MonoBehaviour
+{
+
+	public MicControlC micControl;
+	public float speed = 0.1f;
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (MicControlC.loudness > 0.0f) {
+			Vector3 toRotation = new Vector3 (1.0f, MicControlC.loudness, 1.0f);
+			Quaternion.Lerp(transform.rotation, Quaternion.Euler(toRotation),Time.time * speed);
+			//Debug.Log (MicControlC.loudness);
+		}
+	}
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# OasisAvatarModul
-OasisAvatarModul
