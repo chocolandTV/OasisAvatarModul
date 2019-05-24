e43e4855158830177ec1a4619a5882ca159a6460
chocolandtv <moegenburg@gmx.de>
2019-05-24 23:06:30 +0200

    24.05.19 Lolbunny still isues on movment and no face and eyetracking

---
+++ Assets/KinectBones.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KinectBones : MonoBehaviour
+{
+    public List<GameObject> Lolabunny = new List<GameObject>();
+    public List<GameObject> KinectSensor = new List<GameObject>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < Lolabunny.Count; i++)
+        {
+            Lolabunny[i].transform.rotation=KinectSensor[i].transform.rotation;
+        }
+    }
+}
